8722045afa8b59f5ea4924e57f7b751b1d774853
Brian Faust <hello@brianfaust.de>
2016-11-04 06:35:23 +0200

    first

---
+++ src/HasSlug.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace BrianFaust\Traits;
+
+use Stringy\StaticStringy;
+
+trait HasSlug
+{
+    protected static $slugColumn = 'slug';
+
+    protected static $slugField = 'title';
+
+    public static function bootHasSlug()
+    {
+        static::saving(function ($model) {
+            $model->{static::$slugColumn} = StaticStringy::slugify($model->{static::$slugField});
+        });
+    }
+}
---
+++ src/HasUuid.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace BrianFaust\Traits;
+
+use Rhumsaa\Uuid\Uuid;
+
+trait HasUuid
+{
+    protected static $uuidColumn = 'uuid';
+
+    public static function bootHasUuid()
+    {
+        static::creating(function ($model) {
+            $model->{static::$uuidColumn} = Uuid::uuid4();
+        });
+    }
+}
